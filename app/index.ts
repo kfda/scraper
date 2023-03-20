6e9b904cdf2d0c218da721ebb7d0efeafd7917d8
Kfir Dayan <kfirdayan@Kfirs-MacBook-Pro.local>
2023-03-20 20:25:54 +0200

    first init

---
+++ app/index.ts
@@ -0,0 +1,15 @@
+
+class Scraper {
+
+  scrape(url:string) {
+    
+  }
+
+  parseHTML() {
+    return [{
+      html: "<html></html>",
+      urls: ["https://www.x.com", "https://www.y.com"]
+    }]
+  }
+
+}
